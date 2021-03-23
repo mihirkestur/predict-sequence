48b7eeacf2a3130244f5b7923c3298fe173201ae
Mihir Kestur <mihirkestur@outlook.com>
2021-03-23 18:20:52 +0530

    changes

--- b.py
+++ b.py
@@ -3,10 +3,13 @@ import numpy as np
 from tensorflow import keras
 model = tf.keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
 model.compile(optimizer='sgd',loss='mean_squared_error')
-x = np.array([1,2,3,4,5],dtype=float)
-y = np.array([25,50,75,125,150])
-model.fit(x,y,epochs=3000)
-results = model.predict([6])
+x = np.array([10,9,8,7,6,5,4,3,2,1],dtype=float)
+predict = x[len(x)-1]
+y = np.array([i for i in x[1:]])
+x = x[:len(x)-1]
+
+model.fit(x,y,epochs=2000)
+results = model.predict([[predict]])
 rounded_results = [int(np.round(num)) for num in results]
 print(results)
 print(rounded_results)
